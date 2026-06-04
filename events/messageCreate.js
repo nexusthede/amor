99a09407b0d11d4950731a79a88ccc9fe43bcc0b
nexusthede <ivanrrr76@gmail.com>
2026-06-03 19:46:52 -0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,15 +6,30 @@ module.exports = async (message) => {
     if (!message.guild || message.author.bot) return;
 
     let data = await GuildLB.findOne({ guildId: message.guild.id });
-    if (!data) data = await GuildLB.create({ guildId: message.guild.id });
+
+    if (!data) {
+        data = await GuildLB.create({
+            guildId: message.guild.id,
+            chatLB: { logs: [] },
+            vcLB: { logs: [] },
+            vcSessions: {},
+            vcLive: {}
+        });
+    }
+
+    // 🔥 HARD SAFETY (THIS IS WHAT YOU WERE MISSING)
+    if (!data.chatLB) data.chatLB = { logs: [] };
+    if (!data.chatLB.logs) data.chatLB.logs = [];
 
     data.chatLB.logs.push({
         userId: message.author.id,
         time: Date.now()
     });
 
-    // cleanup
-    data.chatLB.logs = data.chatLB.logs.filter(l => Date.now() - l.time < WEEK);
+    // cleanup old data
+    data.chatLB.logs = data.chatLB.logs.filter(
+        l => Date.now() - l.time < WEEK
+    );
 
     await data.save();
 };
