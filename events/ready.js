6e37fb2154ebf6d03ee199c68c49f1e56e72f3f1
nexusthede <ivanrrr76@gmail.com>
2026-06-03 19:52:21 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,14 @@
-module.exports = (client) => {
+const chatUpdater = require("../utils/chatLeaderboardUpdater");
+const vcUpdater = require("../utils/vcLeaderboardUpdater");
+const vcLiveUpdater = require("../utils/vcLiveUpdater");
+
+module.exports = async (client) => {
     console.log(`${client.user.tag} is online`);
+
+    // 🔥 START ALL SYSTEMS
+    chatUpdater(client);
+    vcUpdater(client);
+    vcLiveUpdater(client);
+
+    console.log("✅ All leaderboard updaters started");
 };
