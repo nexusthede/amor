85f87f0a881c55f90d1bdeb6ea08887ce2793a18
nexusthede <ivanrrr76@gmail.com>
2026-06-03 19:29:50 -0700

    Update setVCLIVE.js

--- commands/setVCLIVE.js
+++ commands/setVCLIVE.js
@@ -1,2 +1,27 @@
-// you will set channel + message ID here
-// bot sends embed then stores IDs in MongoDB
+const GuildLB = require("../models/GuildLB");
+
+module.exports = {
+    name: "setvclive",
+
+    async execute(client, message, args, config) {
+        const channel = message.mentions.channels.first();
+        if (!channel) return message.reply("Mention a channel");
+
+        let data = await GuildLB.findOne({ guildId: message.guild.id });
+        if (!data) data = await GuildLB.create({ guildId: message.guild.id });
+
+        const msg = await channel.send({
+            embeds: [{
+                color: config.embedColor,
+                description: "# 🎙️ Loading voice stats..."
+            }]
+        });
+
+        data.vcLive.channelId = channel.id;
+        data.vcLive.messageId = msg.id;
+
+        await data.save();
+
+        message.reply("VC Live set");
+    }
+};
