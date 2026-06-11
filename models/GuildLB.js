d7e35958ad4148502c05b5b1092a93d5c6be755c
nexusthede <ivanrrr76@gmail.com>
2026-06-11 05:24:37 -0700

    Update GuildLB.js

--- models/GuildLB.js
+++ models/GuildLB.js
@@ -1,29 +1,36 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    guildId: String,
+guildId: String,
 
-    chatLB: {
-        channelId: String,
-        messageId: String,
-        logs: { type: Array, default: [] }
-    },
-
-    vcLB: {
-        channelId: String,
-        messageId: String,
-        logs: { type: Array, default: [] }
-    },
-
-    vcSessions: {
-        type: Object,
-        default: {}
-    },
-
-    vcLive: {
-        channelId: String,
-        messageId: String
+chatLB: {
+    channelId: String,
+    messageId: String,
+    logs: {
+        type: Array,
+        default: []
     }
+},
+
+vcLB: {
+    channelId: String,
+    messageId: String,
+    logs: {
+        type: Array,
+        default: []
+    }
+},
+
+vcSessions: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
+},
+
+vcLive: {
+    channelId: String,
+    messageId: String
+}
+
 });
 
 module.exports = mongoose.model("GuildLB", schema);
